7295c3bb433e156e0ddb413a6d66420541d90503
icarosg <icarodsouza662@gmail.com>
2025-03-08 22:14:44 -0300

    feat: funções e atributos do veículo

--- recarga-carros/cliente/veiculo-cliente.go
+++ recarga-carros/cliente/veiculo-cliente.go
@@ -1,5 +1,5 @@
 package cliente
 
 // definir uma localização fixa para os postos, com longitude e latitude.
-// definir um valor aleatório inicialmente para os veículos e ir sempre alterando de 1 em 1 este valor. Caso a bateria atinga ponto crítico,
+// definir um valor aleatório inicialmente para os veículos e ir sempre alterando este valor. Caso a bateria atinga ponto crítico,
 // fazer uma função para que a longitude e latitude do veículo seja alterada de 1 em 1 para a localização do posto recomendado.
--- recarga-carros/modelo/veiculo.go
+++ recarga-carros/modelo/veiculo.go
@@ -5,35 +5,74 @@ package modelo
 // registro de recargas realizadas.
 
 import (
-	"fmt"
-	"net"
+	"math/rand"
 	"sync"
 )
 
-type Vehicle struct {
+type Veiculo struct {
 	ID         string
 	Latitude   float64
 	Longitude  float64
 	Bateria    float64
 	mu         sync.Mutex
-	IsCharging bool
+	IsCarregando bool
 }
 
-
-
-func Route() {
-	conn, err := net.Dial("tcp", "localhost:8080")
-	if err != nil {
-		fmt.Println("Erro ao conectar ao servidor:", err)
-		return
+func NovoVeiculo(id string, inicialLat, inicialLong float64) Veiculo {
+	return Veiculo{
+		ID:        id,
+		Latitude:  inicialLat,
+		Longitude: inicialLong,
+		Bateria:   100.0, // começa com bateria cheia
 	}
-	defer conn.Close()
+}
 
-	message := "Olá, servidor!"
-	_, err = conn.Write([]byte(message))
-	if err != nil {
-		fmt.Println("Erro ao enviar mensagem:", err)
-		return
+func AtualizarLocalizacao(v *Veiculo) {
+	v.mu.Lock()
+	defer v.mu.Unlock() //o defer, garante que a liberação do bloqueio ocorra de maneira segura e sempre que a função terminar sua execução
+
+	v.Latitude += (rand.Float64() - 0.5) * 0.001
+	v.Longitude += (rand.Float64() - 0.5) * 0.001
+}
+
+func UpdateBAtattery(v *Veiculo) {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+
+	if !v.IsCarregando {
+		// diminui a bateria entre 30.4 e 15.1 por atualização
+		v.Bateria -= rand.Float64()*30.4 + 15.1
+		if v.Bateria < 0 {
+			v.Bateria = 0
+		}
 	}
-	fmt.Println("Mensagem enviada:", message)
+}
+
+func GetNivelBateria(v *Veiculo) float64 {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+
+	return v.Bateria
+}
+
+func GetLocalizacaoVeiculo(v *Veiculo) (float64, float64) {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+
+	return v.Latitude, v.Longitude
+}
+
+func CarregarBateria(v *Veiculo) {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+
+	v.IsCarregando = true
+}
+
+func PararCarregamentoBateria(v *Veiculo) {
+	v.mu.Lock()
+	defer v.mu.Unlock()
+
+	v.IsCarregando = false
+	v.Bateria = 100.0
 }
